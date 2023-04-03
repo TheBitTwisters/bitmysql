278e9069568a812248c855e545f347b23cf13bf2
Michael Angelo <michael01angelo@gmail.com>
2023-04-03 10:55:47 +0800

    refactor: exports error classes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const Queries = require('./queries');
 const BaseModel = require('./models/BaseModel');
 const connect = require('./utils/connect');
+const errors = require('./utils/errors');
 
 module.exports.connect = connect;
 module.exports.Models = { BaseModel };
 module.exports.BaseModel = BaseModel;
 module.exports.Queries = Queries;
+module.exports.errors = errors;

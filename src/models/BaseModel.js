dcaaed0b56563359deadf1fa8cc3d1b0331e1022
Michael Angelo <michael01angelo@gmail.com>
2023-10-29 08:29:21 +0800

    Fix await issue, added async keyword

--- src/models/BaseModel.js
+++ src/models/BaseModel.js
@@ -113,7 +113,7 @@ const BaseModel = class BaseModel {
   save() {
     return new Promise(async (resolve, reject) => {
       try {
-        var jsonData = this.getUpdatedData();
+        var jsonData = await this.getUpdatedData();
         if (Object.keys(jsonData).length > 0) {
           if (this.id > 0) {
             var u = new queries.Update();
@@ -160,7 +160,7 @@ const BaseModel = class BaseModel {
   toJsonData() {
     return {};
   }
-  getUpdatedData() {
+  async getUpdatedData() {
     if (this.id && this.id > 0) {
       var newData = {};
       var oldThis = await BaseModel.get({ id: this.id }));
